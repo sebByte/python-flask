808ba8ac778dbf29506c56757de913d06c69ada4
Fredrik Welander <welandfr@arcada.fi>
2021-10-01 13:57:38 +0300

    add dotenv

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
 
+# Load variables from .env
+load_dotenv()
+print(os.environ.get('HELLO'))
+
+# Create Flask instance
 app = Flask(__name__)
 
+# Default route to /
 @app.route("/")
 def index():
     return "Hello Flask!"
 
+# Run app if called directly
 if __name__ == "__main__":
         app.run()    
