fdc58ff5ab5ad3fa414d0887a2a321dd90c41a49
Seb <70575531+sebByte@users.noreply.github.com>
2021-10-11 17:11:33 +0300

    Lession 8 - Py Flask
    Reach end of lession 8, push to save

--- app/main.py
+++ app/main.py
@@ -1,19 +1,39 @@
+#pylint: disable=no-member
+
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 # Load variables from .env
 load_dotenv()
-print(os.environ.get('HELLO'))
+#print(os.environ.get('HELLO'))
 
 # Create Flask instance
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    updated_at = db.Column(db.DateTime(), default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email())
 
 # Default route to /
 @app.route("/")
 def index():
-    return "Hello Flask!"
+    ret = []
+    for u in User.query.all():
+        ret.append({'email': u.email})
+
+        return jsonify(ret)
+
 
 # Run app if called directly
 if __name__ == "__main__":
-        app.run()
+    app.run()
